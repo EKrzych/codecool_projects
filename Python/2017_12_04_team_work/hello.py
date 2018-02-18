7e5c604cccd4b927925b7539641a7b91c6128f77
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-02-18 20:43:16 +0100

    Cleanup

--- Python/2017_12_04_team_work/hello.py
+++
@@ -1,9 +0,0 @@
-# Create a script file that welcomes the user given to it. 
-# If no name was given, then it welcomes the whole world.
-# Name is provided as the command line argument of the script.
-
-import sys
-if((len(sys.argv))>1):
-    print ("Hello", sys.argv[1])
-else:
-    print ("Hello world")
